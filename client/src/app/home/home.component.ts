1fd7749f2b219ee4b17ea08caf11c7820c1ec020
Mostafa-Rabea <mostafa.rapi3@gmail.com>
2023-11-13 17:10:20 +0200

    Dashboard Design

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css', '../../assets/bootstrap/css/bootstrap.min.css','../../assets/bootstrap/css/style.min.css','../../assets/bootstrap/css/icons.min.css', '../../assets/simplebar/simplebar.min.css','../../assets/@mdi/font/css/materialdesignicons.min.css','../../assets//@iconscout/unicons/css/line.css']
+})
+export class HomeComponent {
+
+}
